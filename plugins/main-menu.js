027dabb31502cd674b0fbfe723bc87544eab7410
𝐁𝐫𝐱𝐳𝐳𝐳 <jxxlznexe@gmail.com>
2025-10-10 10:23:44 -0500

    main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -33,7 +33,7 @@ const defaultMenu = {
 `.trimStart(),
 
 header: '\n╭─〔 🦠 %category 〕─╮',
-  body: '│ ⚙️ %cmd\n',
+  body: '│ ❀ %cmd\n',
   footer: '╰────────────────╯',
   after: '\n⌬ 𝗖𝗬𝗕𝗘𝗥 𝗠𝗘𝗡𝗨 ☠️ - Sistema ejecutado con éxito.'
 }
