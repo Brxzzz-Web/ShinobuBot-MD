a5a562f6fd2c4a4df48d200eacd31a09d269614d
𝐁𝐫𝐱𝐳𝐳𝐳 <jxxlznexe@gmail.com>
2025-10-10 18:56:34 -0500

    handler.js

--- núcleo•clover/handler.js
+++ núcleo•clover/handler.js
@@ -264,12 +264,12 @@ export async function handler(chatUpdate) {
 
   global.dfail = (type, m, usedPrefix, command, conn) => {
     const msg = {
-        rowner: `❀ *Permiso Denegado*\n\n> Solo el *Creador* puede ejecutar este comandó.\n\n❀ Usuario Autorizado: ❀ 𝑺𝒉𝒊𝒏𝒐𝒃𝒖-𝑶𝒇𝒄\n❀ Sistema: root@asTa-bot://omega/core`,
+        rowner: `🛑 *ACCESO RESTRINGIDOΩ*\n\n> Solo el *Creador Supremo* puede ejecutar este protocolo.\n\n🧬 Usuario Autorizado: 👑 𝙏𝙃𝙀 𝘾𝘼𝙍𝙇𝙊𝙎\n🔗 Sistema: root@asTa-bot://omega/core`,
         owner: `⚙️🔒 *MÓDULO DEV: ACCESO BLOQUEADO*\n\n> Esta función está anclada a permisos de *𝙳𝙴𝚂𝙰𝚁𝚁𝙾𝙻𝙻𝙰𝙳𝙾𝚁*.\n\n🧠 Consola de Seguridad: dev@asta.ai/core.sh`,
         premium: `*REQUIERE CUENTA PREMIUM*\n\n> 🚫 Módulo exclusivo para usuarios *𝙑𝙄𝙋 - 𝙋𝙍𝙀𝙈𝙄𝙐𝙈*.\n\n📡 Actualiza tu plan con: */vip*\n⚙️ Estado: denegado`,
         private: `🔒 *SOLO CHAT PRIVADO* 📲\n\n> Este comando no puede ejecutarse en grupos por razones de seguridad.\n\n🧬 Ejecuta este protocolo directamente en el chat privado.`,
         admin: `🛡️ *FUNCIÓN RESTRINGIDA*\n\n> Solo los administradores del *Grupo* tienen acceso.\n\n⚠️ Intento no autorizado.`,
-        unreg: `🧾 *NO REGISTRADO EN EL SISTEMA*\n\n> 🚫 *Acceso denegado:* No puedes usar los comandos sin registrarte.\n\n🔐 Regístrate con: */reg nombre.edad*\n❀ Ejemplo: */reg ShinoDev.21*\n\n> ❀*𝑩𝒐𝒕 𝑶𝒇𝒊𝒄𝒊𝒂𝒍:* 𝑺𝑯𝑰𝑵𝑶𝑩𝑼-𝑩𝑶𝑻 𝑶𝑭𝑪\n\n❀ *𝑺𝒉𝒊𝒏𝒅𝒆𝒗-𝑶𝒇𝒄`,
+        unreg: `🧾 *NO REGISTRADO EN EL SISTEMA*\n\n> 🚫 *Acceso denegado:* No puedes usar los comandos sin registrarte.\n\n🔐 Regístrate con: */reg nombre.edad*\n📍 Ejemplo: */reg Asta.20*\n\n> 🥷🏻 *Instagram oficial del creador del bot  :*\nhttps://www.instagram.com/_carlitos.zx\n\n📂 *Creador del bot:* The Carlos`,
         restrict: `🚷 *FUNCIÓN GLOBALMENTE BLOQUEADA*\n\n> Este comando fue deshabilitado por el *Operador Global* por motivos de seguridad cibernética.\n\n🔧 Módulo: /xvideos`
         }[type];
 if (msg) return m.reply(msg).then(_ => m.react('✖️'))}
